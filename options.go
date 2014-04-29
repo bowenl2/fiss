feb59957ab7e16c20460c1a657ba3d4fb4242efc
Liam Bowen <Liam.Bowen@xls.xerox.com>
2014-04-29 19:12:36 -0400

    Skeleton of options.go

---
+++ options.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/jessevdk/go-flags"
+)
+
+type Options struct {
+	Port uint16 ``
+	Address string ``
+	Root string ``
+}
+
+func printUsage() {
+
+}
+
+func parseOptions() (*Options, error) {
+	
+}
