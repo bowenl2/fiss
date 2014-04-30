f9edb2b007811467079bc10acf795f395743374e
Liam Bowen <LiamBowen@gmail.com>
2014-04-30 17:25:05 -0400

    Option descriptors

--- options.go
+++ options.go
@@ -5,15 +5,17 @@ import (
 )
 
 type Options struct {
-	Port uint16 ``
-	Address string ``
-	Root string ``
-}
-
-func printUsage() {
-
+	Port uint16 `short:"p" long:"port" description:"Local port on which to listen"`
+	Address string `short:"a" long:"address" description:"Address to bind to (0.0.0.0 for all)"`
+	Root string `short:"r" long:"root" description:"Root directory of server"`
 }
 
 func parseOptions() (*Options, error) {
-	
+	options := &Options{}
+	parser := flags.NewParser(options, flags.Default)
+	_, err := parser.Parse()
+	if err != nil {
+		return nil, err
+	}
+	return options, nil
 }
