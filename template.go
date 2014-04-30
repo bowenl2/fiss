d00b2853df691f9df2dbd65c1d21804bb9ce3a6a
Liam Bowen <LiamBowen@gmail.com>
2014-04-30 17:23:00 -0400

    Adding template rendering code

---
+++ template.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"html/template"
+	"os"
+	"io"
+	"path/filepath"
+)
+
+func render(viewName string, viewModel interface{}, w io.Writer) (error) {
+	layoutAsset, err := Asset("layout.html")
+	if err != nil {
+		return err
+	}
+	layoutString := string(layoutAsset)
+
+	contentAsset, err := Asset(viewName)
+	if err != nil {
+		return err
+	}
+	contentString := string(contentAsset)
+
+	tmplFuncs := map[string]interface{}{
+		"fmtsize": func(s int64) string {
+			return ByteSize(s).String()
+		},
+		"relpath": func(f os.FileInfo) string {
+			// Path relative to the base of root of the server
+			return filepath.Join(viewModel.(DirectoryList).Path, f.Name())
+		},
+	}
+
+	tmpl := template.New("template").Funcs(tmplFuncs)
+	tmpl = template.Must(tmpl.Parse(layoutString))
+	tmpl = template.Must(tmpl.Parse(contentString))
+
+	return tmpl.Execute(w, viewModel)
+}
