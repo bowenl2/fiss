2b074d740e3fe89c7bdfdbb6634b9c14509698c0
Liam Bowen <LiamBowen@gmail.com>
2014-05-01 19:55:22 -0400

    Add more options as well as defaults

--- options.go
+++ options.go
@@ -5,9 +5,10 @@ import (
 )
 
 type Options struct {
-	Port uint16 `short:"p" long:"port" description:"Local port on which to listen"`
-	Address string `short:"a" long:"address" description:"Address to bind to (0.0.0.0 for all)"`
-	Root string `short:"r" long:"root" description:"Root directory of server"`
+	Address string `short:"a" long:"address" description:"Address of interface on which to bind" default:"0.0.0.0"`
+	Port uint16 `short:"p" long:"port" description:"Local port on which to listen" default:"8080"`
+	Root string `short:"r" long:"root" description:"Root directory of server" default:"."`
+	Verbose bool `short:"v" long:"verbose" description:"Print absurd amounts of debugging information"`
 }
 
 func parseOptions() (*Options, error) {
