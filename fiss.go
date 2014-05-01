c656dc24549f1b25052242c70b03000d363b8282
Liam Bowen <LiamBowen@gmail.com>
2014-05-01 19:55:09 -0400

    Display what we're serving

--- fiss.go
+++ fiss.go
@@ -13,6 +13,14 @@ func main() {
 	if err != nil {
 		return
 	}
+	absRoot, _ := filepath.Abs(options.Root)
+
+	fmt.Printf(
+		"%s: %s:%d %s\n",
+		os.Args[0],
+		options.Address,
+		options.Port,
+		absRoot)
 
 	http.HandleFunc("/", func(rw http.ResponseWriter, req *http.Request) {
 		p := filepath.Join(options.Root, path.Clean(req.URL.Path))
