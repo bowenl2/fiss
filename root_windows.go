ea8ebd92f3ea50942f0c29fb41ab0a9169b4eff7
Liam Bowen <LiamBowen@gmail.com>
2014-05-07 13:49:44 -0400

    Fix syntax errors and type ambiguity

--- root_windows.go
+++ root_windows.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"github.com/AllenDang/w32"
 	"fmt"
+	"github.com/AllenDang/w32"
 )
 
 func listRoots() []string {
 	driveBitset := w32.GetLogicalDrives()
-	roots = make([]string, 0, 32)
-	for i=0; i<32; i++ {
-		if driveBitset & (1 << i) {
-			append(roots, fmt.Sprintf(`%c:\`, 'A'+i))
+	roots := make([]string, 0, 32)
+	for i := uint(0); i < 32; i++ {
+		if (driveBitset & (1 << i)) != 0 {
+			roots = append(roots, fmt.Sprintf(`%c:\`, 'A'+i))
 		}
 	}
 	return roots
